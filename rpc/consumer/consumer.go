29f2bec17f9629101057d7a3ca1992c29226a918
liunengkai <saya@saya.ac.cn>
2019-06-17 22:43:00 +0800

    增加rpc

---
+++ rpc/consumer/consumer.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"go-start/dao"
+	"log"
+	"net/rpc"
+)
+
+func main() {
+	client, err := rpc.DialHTTP("tcp", "127.0.0.1:8095")
+	if err != nil {
+		log.Fatalln("dailing error: ", err)
+	}
+
+	req := dao.ArithRequest{9, 2}
+	var res dao.ArithResponse
+
+	err = client.Call("Arith.Multiply", req, &res) // 乘法运算
+	if err != nil {
+		log.Fatalln("arith error: ", err)
+	}
+	fmt.Printf("%d * %d = %d\n", req.A, req.B, res.Pro)
+
+	err = client.Call("Arith.Divide", req, &res)
+	if err != nil {
+		log.Fatalln("arith error: ", err)
+	}
+	fmt.Printf("%d / %d, quo is %d, rem is %d\n", req.A, req.B, res.Quo, res.Rem)
+}
---
+++ rpc/provider/provider.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"go-start/dao"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+)
+
+// https://studygolang.com/articles/14336
+
+// 算数运算结构体
+type Arith struct {
+}
+
+// 乘法运算方法
+func (this *Arith) Multiply(req dao.ArithRequest, res *dao.ArithResponse) error {
+	res.Pro = req.A * req.B
+	return nil
+}
+
+// 除法运算方法
+func (this *Arith) Divide(req dao.ArithRequest, res *dao.ArithResponse) error {
+	if req.B == 0 {
+		return errors.New("divide by zero")
+	}
+	res.Quo = req.A / req.B
+	res.Rem = req.A % req.B
+	return nil
+}
+
+func main() {
+	rpc.RegisterName("Arith", new(Arith)) // 注册rpc服务
+	rpc.HandleHTTP()                      // 采用http协议作为rpc载体
+
+	listener, err := net.Listen("tcp", "127.0.0.1:8095")
+	if err != nil {
+		log.Fatalln("ListenTCP error: ", err)
+	}
+
+	fmt.Fprintf(os.Stdout, "%s", "start connection")
+	http.Serve(listener, nil)
+}
